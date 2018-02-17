f021b1ecaedf4d7fe40913eb8e2fa0295bf98a13
SokolovMikhail <miha-a-miha@yandex.ru>
2018-02-17 18:03:07 +0500

    Fix

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -34,11 +34,11 @@ $this->title = 'Темы исков';
 				<tbody>
 					<?$i = 1;?>
 					<?foreach($topics as $item){?>
-					<tr>
+					<tr data-topic-row="">
 						<td style="width: 10%;"><?= $i?></td>
 						<td style="width: 85%;"><a href="/topic/update/?id=<?= $item['id']?>"><?= $item['name']?></a></td>
 						<td><a href="/topic/update/?id=<?= $item['id']?>"><i class="fa fa-pencil clickable text-black" aria-hidden="true" data-toggle="tooltip" title="Редактировать"></i></a></td>
-						<td><a href=""><i class="fa fa-times clickable text-red" aria-hidden="true" data-toggle="tooltip" title="Удалить"></i></a></td>
+						<td><i class="fa fa-times clickable text-red" aria-hidden="true" data-toggle="tooltip" title="Удалить" data-topic-delete="<?= $item['id']?>"></i></td>
 					</tr>							
 					<?$i++;}?>
 				</tbody>
