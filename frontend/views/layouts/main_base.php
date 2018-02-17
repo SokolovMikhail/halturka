f5f3d847c7b36b85cf1990d136a1049e5754f9b5
AndreyShikhov <Andrey.Shikhov94@yandex.ru>
2018-02-17 22:52:25 +0500

    заверстал первую страницу

--- frontend/views/layouts/main_base.php
+++ frontend/views/layouts/main_base.php
@@ -22,32 +22,32 @@ AccountAsset::register($this);
 <body>
     <?php $this->beginBody() ?>
     <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'Juridical',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
-            ];
-            if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {
-                $menuItems[] = [
-                    'label' => 'Выйти (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ];
-            }
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => $menuItems,
-            ]);
-            NavBar::end();
-        ?>
+        // <?php
+            // NavBar::begin([
+                // 'brandLabel' => 'Juridical',
+                // 'brandUrl' => Yii::$app->homeUrl,
+                // 'options' => [
+                    // 'class' => 'navbar-inverse navbar-fixed-top',
+                // ],
+            // ]);
+            // $menuItems = [
+                // ['label' => 'Home', 'url' => ['/site/index']],
+            // ];
+            // if (Yii::$app->user->isGuest) {
+                // $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+            // } else {
+                // $menuItems[] = [
+                    // 'label' => 'Выйти (' . Yii::$app->user->identity->username . ')',
+                    // 'url' => ['/site/logout'],
+                    // 'linkOptions' => ['data-method' => 'post']
+                // ];
+            // }
+            // echo Nav::widget([
+                // 'options' => ['class' => 'navbar-nav navbar-right'],
+                // 'items' => $menuItems,
+            // ]);
+            // NavBar::end();
+        // ?>
         <div class="container">
 			<div class="row">
 				<div class="col-xs-12 mb-20">
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,9 +3,30 @@
 
 $this->title = '';
 ?>
+<div class = "row" > 
+	<div class = " col-md-12">
+		<div class = "text-overtop">СУДА ПУКФЫ</div>
+		
+	</div>	
 
-<select class="selectpicker" data-live-search="true">
-  <option >Hot Dog, Fries and a Soda</option>
-  <option >Burger, Shake and a Smile</option>
-  <option >Sugar, Spice and all things nice</option>
-</select>
+</div>
+<div class ="row">
+	<div class ="col-md-6 col-md-offset-3">
+	 <div class = "text-top">ВЫБЕРИТЕ ТЕМУ ИСКА</div>
+	</div>
+</div>
+
+<div class ="row">
+	<div class ="col-md-6 col-md-offset-3">
+		<div class="form-group form-group-lg">
+		<select class="form-control selectpicker my-form-control" data-live-search="true">
+		<option >алименты</option>
+		<option >гражданские иск</option>
+		<option >арбитражные иски</option>
+		<option >арбитражные уски</option>
+		<option >арбитражные куски</option>
+		<option >арбитражные тески</option>
+		</select>
+	</div>
+</div>
+
