dcacde10cc76b02e8e730b62033a6f774f7c3fac
AndreyShikhov <Andrey.Shikhov94@yandex.ru>
2018-02-18 20:53:07 +0500

    теперь есть последняя страница

--- frontend/views/process/end.php
+++ frontend/views/process/end.php
@@ -11,6 +11,6 @@ $this->title = 'Завершение';
 </div>
 <div class="row">
 	<div class="col-md-6 col-md-offset-3">
-	 <div class="text-top">Проверьте свою почту, ваш иск лежит уже там</div>
+	 <div class="text-top">Проверьте свою почту, ваш иск лежит уже там.</div>
 	</div>
 </div>
